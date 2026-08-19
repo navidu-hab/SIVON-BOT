8c1aff5ff39ff3561b1aea5fcb9327fe34531cc1
navidu-hab <mavishka91@gmail.com>
2026-08-19 00:14:36 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/navidu-hab/SIVON-BOT/blob/main/images/WhatsApp%20Image%202026-08-18%20at%2011.44.08%20PM.jpeg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*HEY BABY👋 SIVON-V1 IS ALIVE NOW 😍*",
+BOT_OWNER: '94726755185',  // Replace with the owner's phone number
 
 
 
